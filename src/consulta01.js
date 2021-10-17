486c8cfe2806ed7f9e56633368539b634b1dc3e5
Francisco Buiza <fbuiper386@g.educaand.es>
2021-10-17 21:35:39 +0200

    Proyecto terminado

--- src/consulta01.js
+++ src/consulta01.js
@@ -1,1 +1,32 @@
+
+db.col01.find( { item: "postcard" } )
+
+//misma consulta usando el operador $eq
+
+db.col01.find( { item: { $eq: "postcard" } } )
+
+db.col01.find( {qty: { $eq: 25} } )
+
+//los que tienen el valor "canvas" en el campo item
+
+db.col01.find( { item: "canvas" } )
+
+//hacer la misma consulta usando el operador $eq
+
+db.col01.find( { item: { $eq: "canvas" } } )
+
+ //los que en el campo qty tienen un valor igual a 20
 db.col01.find( {qty: { $eq: 20} } )
+/*
+{ "_id" : 2, "item" : { "name" : "cd", "code" : "123" }, "qty" : 20, "tags" : [ "B" ] }
+{ "_id" : 5, "item" : { "name" : "mn", "code" : "000" }, "qty" : 20, "tags" : [ [ "A", "B" ], "C" ] }
+*/
+
+//Donde el campo code sea 123
+db.col01.find( { "item.code": "123" })
+
+//Mongo interpreta una busqueda dentro de un array al darse cuenta que se trata de un array
+
+db.col01.find({ tags: "B" })
+
+db.col01.find( { tags: { $eq: [ "A", "B" ] } } )
